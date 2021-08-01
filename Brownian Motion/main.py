80ee255f4098261c28fd67bcb2cd1bf0e09e2ac0
Nick Tran <nt1456@nyu.edu>
2021-08-01 18:44:53 -0400

    updates

--- Brownian Motion/main.py
+++ Brownian Motion/main.py
@@ -1,19 +1,26 @@
 """ 
     Author: Nick Tran
     On: July 27, 2021 
-    Contact: nick.tran@nyu.edu or 248-221-2431
+    Contact Information: nick.tran@nyu.edu or 248-221-2431
 
     About This Program
     ---------------------------------------------
-    This is a model of a 30-year fixed-rate loan
+    This is a Model of a 30-year Fixed-Rate Mortgage
 
-    Time Complexity:    O(n), Theta(1)
-    Space Complexity:   O(n), Theta(1)
+    Time Complexity:    O(1), Theta(1)
+    Space Complexity:   O(1), Theta(1)
+
+    Model Limitations and Assumptions:
+
+    1. People prepaying are only paying double their scheduled principal payment amouunt
+    2. We are not considering the time period, within the month, that constitutes a prepayment
+        - if someone pays 5 days before the first of the next month, would that be considered a prepayment?
+        - what about paying on the first of the month?
 
 """
 
+from sys import intern
 from numpy.testing._private.utils import nulp_diff
-import pandas as pd
 import numpy as np
 import numpy_financial as npf #pip3 install numpy_financial and switch to base:conda environment
 
@@ -49,14 +56,14 @@ class Mortgage(object):
         self.scheduled_InterestPayments = self.setScheduledInterestPayments(period, balance)
         self.prepaidMonths = prepaymentsArr # array for designation for the month borrower prepaid
         self.maxProfit = self.calculateMaxProfit()
-        self.loss = self.calculateLoss()
-        self.totalProfit = self.maxProfit - self.loss
-        self.principal = []
-        self.interest = []
-        self.amortizedPayments = []
-        self.seasonedMonths = period - len(prepaymentsArr)
-        self.paid = [0.0]
+        self.principal = [ppmt for month, ppmt in self.scheduled_PrincipalPayments]
+        self.interest = [ipmt for month, ipmt in self.scheduled_InterestPayments]
         self.owed = [balance]
+        self.amortizedPayments = [interest + principal for interest,principal in zip(self.interest, self.principal)]
+        self.sizeOfList = []
+        self.interestwithPrepayment = self.calculateInterestWithPrepayment()
+        self.totalProfit = self.maxProfit - self.interestwithPrepayment
+        self.seasonedMonths = len(self.owed)
 
     def calculateMaxProfit(self):
         '''This method sums the scheduled interest payments per month'''
@@ -81,28 +88,47 @@ class Mortgage(object):
             tempArr.append((month, float(ipmt)))
         return tempArr
 
-    def calculateLoss(self):
+    def calculateScheduledInterest(self, index, rate, nper, pv):
+        for i in range(index, self.period):
+            ipmt = float(npf.ipmt(rate, i, nper, rate, -pv)) #returns the interest payment for month i
+            self.interest[i] = ipmt
+        return 1
+        
+    def calculateInterestWithPrepayment(self):
         """This function returns the profit loss on a mortgage due to prepayments"""
         if len(self.prepaidMonths) > 0:
-            '''we need to make changes to the amount of principal paid'''
-            for prepaidMonth in self.prepaidMonths:
-                index = self.scheduled_PrincipalPayments.index((prepaidMonth))
-                newPrincipal = self.scheduled_PrincipalPayments[index][1] * 2
-
-            return loss
+            '''if the loan has prepayments, we need to make changes to the amount of principal paid'''
+            for i in range(1, len(self.scheduled_PrincipalPayments)+1):  # i = 1,2,3,...360
+                # Iterate through scheduled principal payments to find the prepaying month
+                for monthIndex in self.prepaidMonths:  # check to see if the the monthIndex matches the prepaymentMonth
+                    if monthIndex == self.scheduled_PrincipalPayments[i-1][0]: #MATCH! 
+                        # for each month specified as prepaid, we double that month's principal payment (self.principal)
+                        double_ppmt = self.scheduled_PrincipalPayments[i-1][1] * 2
+                        old_balance = self.owed[-1]
+                        new_balance = old_balance - double_ppmt
+                        self.owed.append(new_balance) # appends the new balance
+                        self.sizeOfList.append(i)
+                        for x in range(i, self.period+1):  #O(360)
+                            #update the interest owed and the scheduled amortized payments
+                            self.interest[x-1] = float(npf.ipmt(self.periodicRate, x, self.period, -1 * self.owed[-1])) #updating the interest array (time series)
+                            self.amortizedPayments[x-1] = self.interest[x-1] + self.principal[x-1]
+            return sum(payment for payment in self.interest) # self.maxProfit - sum(payment for month, payment in self.scheduled_InterestPayments)
         else:
-            return 0
+            return sum(self.interest)
             
-    
     def __repr__(self) -> str:
-        return repr("Loan ID: " + str(self.loanID) + " Max Profit: " + str((self.maxProfit)))
+
+        return repr("Loan ID: " + str(self.loanID) + "| Balance: " + str(self.balance) \
+                  + "Max Profit: " + str(self.maxProfit) + "| Total Profit (With Prepayments): " + str(self.totalProfit) \
+                     + " Interest: " + str(sum(self.interest)))
+
 
 def main():
-    mortgage1 = Mortgage(1221130638, "Paige", "Some Broker", 360, 100000, 0.03, [])
-    mortgage2 = Mortgage(1221130638, "Ben", "A Broker", 360, 500000, 0.03, [i for i in range(10,20)])
-    mortgage3 = Mortgage(1221130638, "Nick", "Some Broker", 360, 100000, 0.03, [i for i in range(20,30)])
+    mortgage1 = Mortgage(1221130638, "Paige", "Some Broker", 360, 100000, 0.03, [i for i in range(50,100)])
+    mortgage2 = Mortgage(1221130638, "Ben", "A Broker", 360, 500000, 0.03, [])
+    mortgage3 = Mortgage(1221130638, "Nick", "Some Broker", 360, 100000, 0.03, [])
     print(mortgage1)
     print(mortgage2)
     print(mortgage3)
-
 main()
+
